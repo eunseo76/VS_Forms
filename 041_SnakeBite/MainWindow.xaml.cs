4426d41c1110c644fba2101769414484f23598d0
BANG EUNSEO <ekfkawnl7606@naver.com>
2022-06-16 20:39:26 +0900

    시험 하루전

---
+++ 041_SnakeBite/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace _041_SnakeBite
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Game game = new Game();
+            game.Show();
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
